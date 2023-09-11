bfeab7184f0e138b03566f57aaf48141ef5f8ba1
Atemoshok <143033785+Atemoshok@users.noreply.github.com>
2023-09-11 16:38:01 -0400

    Create lab3.sql

---
+++ lab3.sql
@@ -0,0 +1,16 @@
+-- Q2.1
+insert into student(s_email,s_name,major)
+values
+	('s2@jmi.edu','s2','IA'),
+	('s3@jmu.edu','s3','ISAT'),
+	('s4@jmu.edu','s4','ISAT');
+
+select * from student
+
+--Q2.2
+insert into professor(p_email,p_name,office)
+values
+	('p1@jmi.edu','p1','o1'),
+	('p2@jmu.edu','p2','o2');
+
+select * from professor
