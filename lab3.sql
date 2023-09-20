84a07b0662e5e5ae178ded7d7f54fc3a19f3885d
Atemoshok <143033785+Atemoshok@users.noreply.github.com>
2023-09-20 16:03:50 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -52,3 +52,23 @@ where p_email = 'p1@jmu.edu'
 delete from professor
 where p_email= 'p1@jmu.edu'
 --You have to update new data before entering new ones so course first
+--Q2.7
+select * from enroll
+--Q2.8
+select c_number,count(*) as num_students
+from enroll
+group by c_number
+order by num_students desc
+limit 1
+--Q.2.9
+select professor.p_name,course.c_name
+from professor
+inner join course
+on professor.p_email=course.p_email
+--Q.2.10
+select professor.p_name, count(course.c_number) as num_courses_taught
+from professor
+inner join course on professor.p_email=course.p_email
+group by professor.p_name
+order by num_courses_taught desc
+limit 1
